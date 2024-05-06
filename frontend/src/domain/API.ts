eba0cf8edb07257635bb766e90558100ef307f80
Jose Cardona <jpcardonamena@gmail.com>
2024-05-06 23:11:29 +0200

    Added more custom functions to API.ts

--- frontend/src/domain/API.ts
+++ frontend/src/domain/API.ts
@@ -2,16 +2,28 @@ import Book from "./Book";
 
 const API_URL = 'http://localhost:4730'; // replace with your backend URL
 
+
+//Standard CRUD Operations
+
+/**
+ * Get all books from the backend
+ */
 export const getAllBooks = async () => {
     const response = await fetch(`${API_URL}/books`);
     return response.json();
 };
 
+/**
+ * Get a single book from the backend
+ */
 export const getBook = async (isbn: string) => {
     const response = await fetch(`${API_URL}/books/${isbn}`);
     return response.json();
 };
 
+/**
+ * Create a new book
+ */
 export const createBook = async (book: Book) => {
     const response = await fetch(`${API_URL}/books`, {
         method: 'POST',
@@ -23,6 +35,9 @@ export const createBook = async (book: Book) => {
     return response.json();
 };
 
+/**
+ * Update a book
+ */
 export const updateBook = async (book: Book) => {
     const response = await fetch(`${API_URL}/books/${book.isbn}`, {
         method: 'PUT',
@@ -34,9 +49,46 @@ export const updateBook = async (book: Book) => {
     return response.json();
 };
 
+/**
+ * Delete a book
+ */
 export const deleteBook = async (isbn: string) => {
     const response = await fetch(`${API_URL}/books/${isbn}`, {
         method: 'DELETE',
     });
     return response.json();
 };
+
+//Additional Operations
+
+/**
+ * Get all books sorted by title in descending order
+ */
+export const getBooksSortedByTitleDescending = async () => {
+    const response = await fetch(`${API_URL}//books?_sort=title&_order=desc`);
+    return response.json();
+}
+
+/**
+ * Get all books sorted by title in ascending order
+ */
+export const getBooksSortedByTitleAscending = async () => {
+    const response = await fetch(`${API_URL}//books?_sort=title`);
+    return response.json();
+}
+
+/**
+ * Get all books sorted by author in descending order
+ */
+export const getBooksSortedByAuthorDescending = async () => {
+    const response = await fetch(`${API_URL}//books?_sort=author&_order=desc`);
+    return response.json();
+}
+
+/**
+ * Get all books sorted by author in ascending order
+ */
+export const getBooksSortedByAuthorAscending = async () => {
+    const response = await fetch(`${API_URL}//books?_sort=author`);
+    return response.json();
+}
