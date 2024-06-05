ff310fe37909bafddc82efffff3715f3e85b5ea4
Jose Cardona <jpcardonamena@gmail.com>
2024-06-05 19:52:38 +0200

    Surrounded functions in try/catch blocks

--- src/domain/API.ts
+++ src/domain/API.ts
@@ -10,54 +10,80 @@ const API_URL = 'http://localhost:4730'; // replace with your backend URL
  * Get all books from the backend
  */
 export const getAllBooks = async () => {
-    const response = await fetch(`${API_URL}/books`);
-    return response.json();
+    try {
+        const response = await fetch(`${API_URL}/books`);
+        return response.json();
+    } catch (error) {
+        console.error(error);
+        throw error;
+    }
 };
 
 /**
  * Get a single book from the backend
  */
 export const getBook = async (isbn: string) => {
-    const response = await fetch(`${API_URL}/books/${isbn}`);
-    return response.json();
+    try {
+        const response = await fetch(`${API_URL}/books/${isbn}`);
+        return response.json();
+    } catch (error) {
+        console.error(error);
+        throw error;
+    }
+
 };
 
 /**
  * Create a new book
  */
 export const createBook = async (book: Book) => {
-    const response = await fetch(`${API_URL}/books`, {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json',
-        },
-        body: JSON.stringify(book),
-    });
-    return response.json();
+    try{
+        const response = await fetch(`${API_URL}/books`, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(book),
+        });
+        return response.json();
+    }catch(error){
+        console.error(error);
+        throw error;
+    }
 };
 
 /**
  * Update a book
  */
 export const updateBook = async (book: Book) => {
-    const response = await fetch(`${API_URL}/books/${book.isbn}`, {
-        method: 'PUT',
-        headers: {
-            'Content-Type': 'application/json',
-        },
-        body: JSON.stringify(book),
-    });
-    return response.json();
+    try {
+        const response = await fetch(`${API_URL}/books/${book.isbn}`, {
+            method: 'PUT',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(book),
+        });
+        return response.json();
+    }catch(error){
+        console.error(error);
+        throw error;
+    }
 };
 
 /**
  * Delete a book
  */
 export const deleteBook = async (isbn: string) => {
-    const response = await fetch(`${API_URL}/books/${isbn}`, {
-        method: 'DELETE',
-    });
-    return response.json();
+    try {
+        const response = await fetch(`${API_URL}/books/${isbn}`, {
+            method: 'DELETE',
+        });
+        return response.json();
+    }catch(error){
+        console.error(error);
+        throw error;
+    }
 };
 
 //--------------Additional Operations------------------
@@ -66,72 +92,117 @@ export const deleteBook = async (isbn: string) => {
  * Get a specific amount of books
  */
 export const getFirstBooks = async (count: number) => {
-    const response = await fetch(`${API_URL}/books?_limit=${count}`);
-    return response.json();
-
+    try {
+        const response = await fetch(`${API_URL}/books?_limit=${count}`);
+        return response.json();
+    }catch(error){
+        console.error(error);
+        throw error;
+    }
 }
 
 /**
  * Get Books by page
  */
 export const getBooksByPage = async (page: number, limit: number) => {
-    const response = await fetch(`${API_URL}/books?_page=${page}&_limit=${limit}`);
-    return response.json();
+    try {
+        const response = await fetch(`${API_URL}/books?_page=${page}&_limit=${limit}`);
+        return response.json();
+    }catch(error){
+        console.error(error);
+        throw error;
+    }
 }
 
 /**
  * Get all books sorted by title in descending order
  */
 export const getBooksSortedByTitleDescending = async () => {
-    const response = await fetch(`${API_URL}//books?_sort=title&_order=desc`);
-    return response.json();
+    try {
+        const response = await fetch(`${API_URL}/books?_sort=title&_order=desc`);
+        return response.json();
+    }catch(error) {
+        console.error(error);
+        throw error;
+    }
 }
 
 /**
  * Get all books sorted by title in ascending order
  */
 export const getBooksSortedByTitleAscending = async () => {
-    const response = await fetch(`${API_URL}//books?_sort=title`);
-    return response.json();
+    try {
+        const response = await fetch(`${API_URL}/books?_sort=title`);
+        return response.json();
+    }catch(error) {
+        console.error(error);
+        throw error;
+    }
 }
 
 /**
  * Get all books sorted by author in descending order
  */
 export const getBooksSortedByAuthorDescending = async () => {
-    const response = await fetch(`${API_URL}//books?_sort=author&_order=desc`);
-    return response.json();
+    try {
+        const response = await fetch(`${API_URL}/books?_sort=author&_order=desc`);
+        return response.json();
+    }catch(error) {
+        console.error(error);
+        throw error;
+    }
 }
 
 /**
  * Get all books sorted by author in ascending order
  */
 export const getBooksSortedByAuthorAscending = async () => {
-    const response = await fetch(`${API_URL}//books?_sort=author`);
-    return response.json();
+    try {
+        const response = await fetch(`${API_URL}/books?_sort=author`);
+        return response.json();
+    }catch(error) {
+        console.error(error);
+        throw error;
+    }
 }
 
 //---------------Log in / Register---------------------
 
 
 export const login = async (user: User) => {
-    const response = await fetch(`${API_URL}/login}`, {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json',
-        },
-        body: JSON.stringify(user),
-    });
-    return response.json();
+    try {
+        const response = await fetch(`${API_URL}/login`, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(user),
+        });
+        if (!response.ok) {
+            throw new Error(response.status.toString() + ": " + response.statusText)
+        }
+        return response.json();
+    }catch(error){
+        console.error(error);
+        throw error;
+    }
 }
 
-export const register = async (user: User) => {
-    const response = await fetch(`${API_URL}/register}`, {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json',
-        },
-        body: JSON.stringify(user),
-    });
-    return response.json();
+export const registerUser = async (user: User) => {
+    try {
+        const response = await fetch(`${API_URL}/register`, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(user),
+        });
+        if (!response.ok) {
+            throw new Error(response.status.toString() + ": " + response.statusText)
+        }
+        return response.json();
+    }catch(error){
+        console.error(error);
+        throw error;
+    }
 }
