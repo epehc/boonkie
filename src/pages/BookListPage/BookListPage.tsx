8e9a92b5d817bc2a481aa7a1af03749ca01cb237
Jose Cardona <jpcardonamena@gmail.com>
2024-06-05 19:59:02 +0200

    Improved Table structure by adding TableHead and TableBody

--- src/pages/BookListPage/BookListPage.tsx
+++ src/pages/BookListPage/BookListPage.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import {TableContainer, Table, TableCell, TableRow, Box, Button} from "@mui/material";
+import {TableContainer, Table, TableCell, TableRow, Box, Button, TableBody, TableHead} from "@mui/material";
 import Book from "../../domain/Book";
 import BookListPageRow from "../../components/BookListPageRow/BookListPageRow";
 import Pagination from "@mui/material/Pagination";
@@ -19,8 +19,6 @@ const BookListPage: React.FC<BookTableProps> = ({books, page, loggedIn, setPage,
     const theme = useTheme();
     const navigate = useNavigate();
 
-    const tempLoggedIn = true;
-
     const handlePageChange = (event: React.ChangeEvent<unknown>, value: number) => {
         setPage(value)
     }
@@ -36,24 +34,27 @@ const BookListPage: React.FC<BookTableProps> = ({books, page, loggedIn, setPage,
                     <Button variant="contained" color="secondary" onClick={handleNavigateCreateBookPage} sx={{marginTop:"20px", marginBottom:"20px"}}>+ Add new Book</Button>
                 }
                 <Table>
-                    <TableRow sx={{position: "sticky", top: 0, bgcolor: theme.palette.background.paper, zIndex: 1}}>
-                        <TableCell><b>COVER</b></TableCell>
-                        <TableCell><b>TITLE</b></TableCell>
-                        <TableCell><b>AUTHOR</b></TableCell>
-                        <TableCell><b>PUBLISHER</b></TableCell>
-                        <TableCell><b>PRICE</b></TableCell>
-                        <TableCell><b>LIKE</b></TableCell>
-                        {loggedIn &&
-                            <>
-                                <TableCell><b>EDIT</b></TableCell>
-                                <TableCell><b>DELETE</b></TableCell>
-                            </>
-                        }
-
-                    </TableRow>
-                    {books.map(book => (
-                        <BookListPageRow key={book.id} book={book} loggedIn={loggedIn} refresh={refresh}/>
-                    ))}
+                    <TableHead sx={{position: "sticky", top: 60, bgcolor: theme.palette.background.paper, zIndex: 1}}>
+                        <TableRow >
+                            <TableCell><b>COVER</b></TableCell>
+                            <TableCell><b>TITLE</b></TableCell>
+                            <TableCell><b>AUTHOR</b></TableCell>
+                            <TableCell><b>PUBLISHER</b></TableCell>
+                            <TableCell><b>PRICE</b></TableCell>
+                            <TableCell><b>LIKE</b></TableCell>
+                            {loggedIn &&
+                                <>
+                                    <TableCell><b>EDIT</b></TableCell>
+                                    <TableCell><b>DELETE</b></TableCell>
+                                </>
+                            }
+                        </TableRow>
+                    </TableHead>
+                    <TableBody>
+                        {books.map(book => (
+                            <BookListPageRow key={book.id} book={book} loggedIn={loggedIn} refresh={refresh}/>
+                        ))}
+                    </TableBody>
                 </Table>
                 <Pagination count={26} page={page} onChange={handlePageChange} sx={{display: 'flex', justifyContent: 'center',
                     alignItems: 'center', margin: 2}}/>
