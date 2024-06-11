e04a53c593a6c85cf391d42c8e93283e1e1f6e5f
Jose Cardona <jpcardonamena@gmail.com>
2024-06-11 14:17:29 +0200

    implemented use of alerts, and managed logic for displaying Tablecells depending on user role

--- src/components/BookListPageRow/BookListPageRow.tsx
+++ src/components/BookListPageRow/BookListPageRow.tsx
@@ -1,26 +1,34 @@
-import React, {useState} from "react";
-import {IconButton, TableCell, TableRow} from "@mui/material";
+import React, {useEffect, useState} from "react";
+import {Alert, Button, Collapse, IconButton, TableCell, TableRow} from "@mui/material";
 import Book from "../../domain/Book";
 import AltCover from "../../assets/noLogo.png";
 import FavoriteBorderIcon from '@mui/icons-material/FavoriteBorder';
 import FavoriteIcon from '@mui/icons-material/Favorite';
 import EditIcon from '@mui/icons-material/Edit';
 import DeleteIcon from '@mui/icons-material/Delete';
+import AddShoppingCartIcon from '@mui/icons-material/AddShoppingCart';
 import {useNavigate} from "react-router-dom";
-import {deleteBook} from "../../domain/API";
+import {deleteBook, updateBook} from "../../domain/API";
 import DeleteBookModal from "./DeleteBookModal";
+import {useSelector} from "react-redux";
+import {RootState} from "../../state/store";
+import CloseIcon from "@mui/icons-material/Close";
 
 interface BookTableRowProps {
     book: Book;
-    loggedIn: boolean;
     refresh: () => void;
 }
 
-const BookListPageRow: React.FC<BookTableRowProps> = ({ book, loggedIn, refresh }) => {
+const BookListPageRow: React.FC<BookTableRowProps> = ({ book, refresh }) => {
     const [likes, setLikes] = useState<number>(0);
     const [deleteModalOpen, setDeleteModalOpen] = useState<boolean>(false);
+    const [updateSuccess, setUpdateSuccess] = useState<boolean>(false);
+    const [addedToCart, setAddedToCart] = useState<boolean>(false);
+    const [cartIconDisabled, setCartIconDisabled] = useState<boolean>(false);
 
     const navigate = useNavigate();
+
+    const isAdmin = useSelector((state: RootState) => state.login.isAdmin)
 
 
     const handleSelectBook = () => {
@@ -32,8 +40,24 @@ const BookListPageRow: React.FC<BookTableRowProps> = ({ book, loggedIn, refresh 
         setLikes(likes+1);
     };
 
+    const handleDeleteAlertClose = () => {
+        setUpdateSuccess(false)
+        refresh()
+    }
+
+    const handleAddToCartAlertClose = () => {
+        setAddedToCart(false)
+    }
+
     const handleDeleteModal = () => {
         setDeleteModalOpen(!deleteModalOpen);
+    }
+
+    const handleAddBookToCart = () => {
+        book.userId = 2
+        updateBook(book)
+        setCartIconDisabled(true)
+        setAddedToCart(true)
     }
 
     const handleEditBookModal2 = () => {
@@ -43,15 +67,47 @@ const BookListPageRow: React.FC<BookTableRowProps> = ({ book, loggedIn, refresh 
     const handleDeleteBook = () => {
         deleteBook(book.isbn).then( () => {
             handleDeleteModal();
-            navigate("/")
-            refresh();
+            setUpdateSuccess(true)
+            //navigate("/books")
+            //refresh();
         } ).catch( (error) => {
             console.error("Error deleting book", error)
         })
     }
 
+    useEffect(() => {
+        if(book.userId !== 1) {
+            setCartIconDisabled(true)
+        }
+    }, [book.userId])
+
     return (
         <>
+            {addedToCart &&
+                <TableRow>
+                    <TableCell colSpan={7}>
+                        <Collapse sx={{display: 'flex', justifyContent: 'flex-end', width: '100%', flexGrow: 1}} in={addedToCart}>
+                            <Alert variant="filled" severity="success" onClose={handleAddToCartAlertClose} action={
+                                <>
+                                    <Button color="inherit" size="small" onClick={() => navigate('/cart')}>
+                                        <u>Go to cart</u>
+                                    </Button>
+                                    <IconButton
+                                        aria-label="close"
+                                        color="inherit"
+                                        size="small"
+                                        onClick={handleAddToCartAlertClose}
+                                    >
+                                        <CloseIcon fontSize="inherit" />
+                                    </IconButton>
+                                </>
+                            }>
+                                Book Added to cart!
+                            </Alert>
+                        </Collapse>
+                    </TableCell>
+                </TableRow>
+            }
             <TableRow>
                 <TableCell onClick={handleSelectBook}>
                     {book.cover === "" ? <img src={AltCover} alt="No Cover" style={{margin: 20,width: 100, height: 116}}/> : <img src={book.cover} alt="Cover" style={{width: 150, height: 175}}/>}
@@ -65,7 +121,7 @@ const BookListPageRow: React.FC<BookTableRowProps> = ({ book, loggedIn, refresh 
                         { likes === 0 ? <FavoriteBorderIcon/> : <><FavoriteIcon sx={{color: "red"}}/>{likes}</> }
                     </IconButton>
                 </TableCell>
-                {loggedIn &&
+                {isAdmin &&
                     <>
                         <TableCell>
                             <IconButton onClick={handleEditBookModal2}><EditIcon/></IconButton>
@@ -75,7 +131,24 @@ const BookListPageRow: React.FC<BookTableRowProps> = ({ book, loggedIn, refresh 
                         </TableCell>
                     </>
                 }
+                {!isAdmin &&
+                    <TableCell>
+                        <IconButton disabled={cartIconDisabled} onClick={handleAddBookToCart}><AddShoppingCartIcon/></IconButton>
+                    </TableCell>
+                }
             </TableRow>
+            {updateSuccess &&
+                <TableRow>
+                    <TableCell colSpan={8}>
+                        <Collapse sx={{display: 'flex', justifyContent: 'flex-end', width: '100%', flexGrow: 1}} in={updateSuccess}>
+                            <Alert variant="filled" severity="warning" onClose={handleDeleteAlertClose}>
+                                Book deleted successfully!
+                            </Alert>
+                        </Collapse>
+                    </TableCell>
+                </TableRow>
+            }
+
             <DeleteBookModal open={deleteModalOpen} book={book} onClose={handleDeleteModal} onDeleteBook={handleDeleteBook}/>
         </>
 
