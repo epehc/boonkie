25f1cf777474b433376636d207597dc183bd3985
Jose Cardona <jpcardonamena@gmail.com>
2024-06-11 13:58:35 +0200

    Made aesthetic changes and added add to cart button in bookpage

--- src/pages/BookPage.tsx
+++ src/pages/BookPage.tsx
@@ -1,18 +1,32 @@
 import React, {useEffect, useState} from "react";
 import Grid from "@mui/material/Unstable_Grid2";
-import {Box, Button, Divider, Stack, Typography} from "@mui/material";
+import {Alert, Box, Button, Collapse, Divider, IconButton, Stack, Typography} from "@mui/material";
 import { useTheme } from '@mui/material/styles';
 import Book from "../domain/Book";
 import {useNavigate, useParams} from "react-router-dom";
 import {pawBook} from "../domain/pawBook";
-import {getBook} from "../domain/API";
+import {getBook, updateBook} from "../domain/API";
 import AltCover from "../assets/noLogo.png";
+import ShoppingCartIcon from "@mui/icons-material/ShoppingCart";
+import CloseIcon from "@mui/icons-material/Close";
+import {useSelector} from "react-redux";
+import {RootState} from "../state/store";
 
 const BookPage = () => {
-    const theme = useTheme(); // Access the theme to use its values directly in style props if needed
-    const [book, setBook] = useState<Book>(pawBook); // [1
+    const theme = useTheme();
+    const [book, setBook] = useState<Book>(pawBook);
+    const [updateSuccess, setUpdateSuccess] = useState<boolean>(false);
+    const [cartButtonDisabled, setCartButtonDisabled] = useState<boolean>(false);
+
+    const isAdmin = useSelector((state: RootState) => state.login.isAdmin)
+
     const navigate = useNavigate();
-    const { id } = useParams() as { id: string };
+    const { id } = useParams() as { id: string }
+
+    const handleCloseAlert = () => {
+        setUpdateSuccess(false)
+
+    }
 
     const fetchBook = async () => {
         try {
@@ -24,38 +38,71 @@ const BookPage = () => {
 
     }
 
+    const handleAddBookToCart = () => {
+        book.userId = 2
+        updateBook(book)
+        setUpdateSuccess(true)
+        setCartButtonDisabled(true)
+    }
+
+    const handleNavigateToCart = () => {
+        navigate('/cart')
+
+    }
+
     useEffect(() => {
-        fetchBook()
+        fetchBook().then(() => console.log("Book fetched"))
     }, [])
 
     return (
-        <Grid container spacing={2} style={{ marginTop: '20px', backgroundColor: theme.palette.background.default }}>
-            <Grid xs={2} />
-            <Grid xs={3}>
-                <Stack spacing={2}>
-                    <Box sx={{display: 'flex', justifyContent: 'center'}}>
-                        <Button sx= {{width: '80%'}} variant="text" color="primary" onClick={() => navigate('/')}>Back to Books</Button>
-                    </Box>
-                    {book.cover === "" ? <img src={AltCover} alt="No Cover" style={{ maxWidth: '100%', height: 'auto' }}/> : <img src={book.cover} alt="Cover" style={{ maxWidth: '100%', height: 'auto' }}/>}
-                </Stack>
-            </Grid>
-            <Grid xs={5}>
-                <Stack spacing={2}>
-                    <Typography variant="h4" color="textPrimary">{book.title}</Typography>
-                    <Typography variant="h6" color="textSecondary">{book.subtitle}</Typography>
-                    <Typography variant="subtitle1" style={{ color: theme.palette.secondary.dark }}>by {book.author}</Typography>
-                    <Divider />
-                    <Typography variant="body1">{book.publisher}</Typography>
-                    <Typography variant="body2">ISBN: {book.isbn}</Typography>
-                    <Typography variant="body2">{book.numPages} pages</Typography>
-                    <Typography variant="h5" color="primary">Price: {book.price}</Typography>
-                    <Divider />
-                    <Typography variant="subtitle1">About this book:</Typography>
-                    <Typography variant="body1" sx={{marginBottom: '20px'}}>{book.abstract}</Typography>
-                </Stack>
-            </Grid>
-            <Grid xs={2} />
-        </Grid>
+        <Box sx={{display: 'flex', justifyContent: 'center', height: '60vh'}}>
+            <Box sx={{display: 'flex', justifyContent: 'center',  width: '70%'}}>
+                <Grid container spacing={15} style={{ marginTop: '20px', marginBottom: '20px', backgroundColor: theme.palette.background.default }}>
+                    <Grid xs={4}>
+                        <Button sx= {{width: '100%', marginBottom: '20px'}} variant="text" color="primary" onClick={() => navigate('/books')}><u>Back to Books</u></Button>
+                        {book.cover === "" ? <img src={AltCover} alt="No Cover" style={{ maxWidth: '100%', height: 'auto' }}/> : <img src={book.cover} alt="Cover" style={{ maxWidth: '100%', height: 'auto' }}/>}
+                    </Grid>
+                    <Grid xs={8}>
+                        <Stack spacing={2}>
+                            <Typography variant="h4" color="textPrimary">{book.title}</Typography>
+                            <Typography variant="h6" color="textSecondary">{book.subtitle}</Typography>
+                            <Typography variant="subtitle1" style={{ color: theme.palette.secondary.dark }}>by {book.author}</Typography>
+                            <Divider />
+                            <Typography variant="body1">{book.publisher}</Typography>
+                            <Typography variant="body2">ISBN: {book.isbn}</Typography>
+                            <Typography variant="body2">{book.numPages} pages</Typography>
+                            <Typography variant="h5" color="primary">Price: {book.price}</Typography>
+                            <Divider />
+                            <Typography variant="subtitle1">About this book:</Typography>
+                            <Typography variant="body1" sx={{marginBottom: '20px'}}>{book.abstract}</Typography>
+                            {!isAdmin &&
+                                <Button disabled={cartButtonDisabled} variant="contained" color="primary" startIcon={<ShoppingCartIcon/>} onClick={handleAddBookToCart}>Add to Cart</Button>
+                            }
+                            <Collapse in={updateSuccess}>
+                                <Alert severity="success" variant="filled"  onClose={handleCloseAlert} action={
+                                    <>
+                                        <Button color="inherit" size="small" onClick={handleNavigateToCart}>
+                                            <u>Go to cart</u>
+                                        </Button>
+                                        <IconButton
+                                            aria-label="close"
+                                            color="inherit"
+                                            size="small"
+                                            onClick={handleCloseAlert}
+                                        >
+                                            <CloseIcon fontSize="inherit" />
+                                        </IconButton>
+                                    </>
+                                }>
+                                    Book was added to cart
+                                </Alert>
+                            </Collapse>
+                        </Stack>
+                    </Grid>
+                </Grid>
+            </Box>
+        </Box>
+
     );
 };
 
--- src/pages/CreateBookPage.tsx
+++ src/pages/CreateBookPage.tsx
@@ -1,38 +1,33 @@
 import React, {useState} from 'react';
 import { useForm } from 'react-hook-form';
-import {Button, TextField, Box, InputAdornment, ButtonGroup} from '@mui/material';
+import {Button, TextField, Box, InputAdornment, ButtonGroup, Typography} from '@mui/material';
 import {useNavigate} from "react-router-dom";
 import Grid from "@mui/material/Unstable_Grid2";
-import NewBook from "../domain/NewBook";
 import Book from "../domain/Book";
 import {createBook} from "../domain/API";
 
-interface CreateBookPageProps {
-    refresh: () => void
-}
-
-const CreateBookPage: React.FC<CreateBookPageProps> = ({refresh}) => {
-    const { register, handleSubmit, formState: { errors } } = useForm<NewBook>();
+const CreateBookPage = () => {
+    const { register, handleSubmit, formState: { errors }, watch } = useForm<Book>();
     const [counter, setCounter] = useState(1)
 
     const navigate = useNavigate();
+    const isbn = watch("isbn");
 
-    const onSubmit = (data: NewBook) => {
+    const onSubmit = (data: Book) => {
         // Prepend the "$" symbol to the price value
         data.price = `$${data.price}`;
 
         const newBook: Book = {
             ...data,
-            id: counter.toString(),
-            isbn: counter.toString(),
-            userId: 0
+            id: data.isbn,
+            userId: 1
         }
         try{
             createBook(newBook).then( () => {
                 console.log(newBook);
                 setCounter(counter+1)
-                navigate(`/books/${newBook.id}`)
-                refresh();
+                navigate(`/books/${newBook.isbn}`)
+
             }).catch( (error) => {
                 console.error("Error creating book", error)
             })
@@ -46,49 +41,61 @@ const CreateBookPage: React.FC<CreateBookPageProps> = ({refresh}) => {
     }
 
     return (
-        <Box sx={{display: "flex", justifyContent:"center", marginTop: "20px", marginBottom:"20px"}}>
+        <Box sx={{display: "flex", justifyContent:"center", marginTop: "10vh", marginBottom:"10vh"}}>
             <Box sx={{width: "60%", bgcolor: 'background.paper', borderRadius: '1.5rem', padding: '30px'}}>
+                <Typography variant='h4' sx={{marginBottom: '20px'}}><b>Create a new book</b></Typography>
                 <form onSubmit={handleSubmit(onSubmit)}>
                     <Grid container spacing={2} sx={{width: '100%', marginTop: '20px', marginBottom: '20px'}}>
                         <Grid xs={6}>
-                            <TextField {...register("title", {required: true})} label="Title" fullWidth/>
-                            {errors.title && <p>This field is required</p>}
+                            <TextField {...register("title", {required: true})} label="Title*" fullWidth/>
+                            {errors.title && <Typography variant='body2' sx={{color:'red'}}>* This field is required</Typography>}
                         </Grid>
                         <Grid xs={6}>
-                            <TextField {...register("subtitle", {required: true})} label="Subtitle" fullWidth />
-                            {errors.title && <p>This field is required</p>}
+                            <TextField {...register("subtitle")} label="Subtitle" fullWidth />
                         </Grid>
                     </Grid>
                     <Grid container spacing={2} sx={{width: '100%', marginTop: '20px', marginBottom: '20px'}}>
                         <Grid xs={6}>
-                            <TextField {...register("author", {required: true})} label="Author" fullWidth/>
-                            {errors.author && <p>This field is required</p>}
+                            <TextField {...register("author")} label="Author" fullWidth/>
                         </Grid>
                         <Grid xs={6}>
-                            <TextField {...register("publisher", {required: true})} label="Publisher" fullWidth/>
-                            {errors.publisher && <p>This field is required</p>}
+                            <TextField {...register("publisher")} label="Publisher" fullWidth/>
+                        </Grid>
+                    </Grid>
+                    <Grid container spacing={2} sx={{width: '100%', marginTop: '20px', marginBottom: '20px'}}>
+                        <Grid xs={6}>
+                            <TextField {...register("isbn", {required: true})} label="ISBN*" fullWidth/>
+                            {errors.title && <Typography variant='body2' sx={{color:'red'}}>* This field is required</Typography>}
+                        </Grid>
+                        <Grid xs={6}>
+                            <TextField value={isbn} label="ID" fullWidth InputLabelProps={{ shrink: !!isbn }} />
                         </Grid>
                     </Grid>
                     <Grid container spacing={2} sx={{width: '100%', marginTop: '20px', marginBottom: '20px'}}>
                         <Grid xs={3}>
-                            <TextField {...register("price", {required: true})} label="Price"
+                            <TextField {...register("price")} label="Price"
                                        fullWidth InputProps={{startAdornment: <InputAdornment position="start">$</InputAdornment>,
                             }}/>
-                            {errors.price && <p>This field is required</p>}
                         </Grid>
                         <Grid xs={3}>
-                            <TextField {...register("numPages", {required: true, valueAsNumber: true})} label="Number of Pages"
+                            <TextField {...register("numPages", {valueAsNumber: true})} label="Number of Pages"
                                        type="number" fullWidth/>
-                            {errors.numPages && <p>This field is required</p>}
                         </Grid>
                         <Grid xs={6}>
                             <TextField {...register("cover")} label="Cover URL" fullWidth/>
                         </Grid>
                     </Grid>
-                    <TextField {...register("abstract")} label="About" fullWidth multiline rows={3}/>
-                    <ButtonGroup sx={{width: '15%', display: 'flex', justifyContent: 'space-between', marginTop: '20px'}}>
-                        <Button type="submit" variant="contained" color="primary">Submit</Button>
-                        <Button onClick={handleCancel} variant="contained" color="secondary" sx={{marginLeft: "10px"}}>Cancel</Button>
+                    <Grid container spacing={2} sx={{width: '100%', marginTop: '20px', marginBottom: '20px'}}>
+                        <Grid xs={12}>
+                            <TextField {...register("abstract")} label="About" fullWidth multiline rows={3}/>
+                        </Grid>
+                    </Grid>
+                    <Typography variant='body2' sx={{marginTop: '20px', color: 'primary'}}>
+                        fields marked with * are required. ISBN and ID will be identical.
+                    </Typography>
+                    <ButtonGroup sx={{ display: 'flex', justifyContent: 'center', marginTop: '20px'}}>
+                        <Button type="submit" variant="contained" color="primary" sx={{marginRight: "40px"}}>Submit</Button>
+                        <Button onClick={handleCancel} variant="contained" color="secondary" >Cancel</Button>
                     </ButtonGroup>
                 </form>
             </Box>
