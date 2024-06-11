76ffa818b3b2ad64e44281717f7d8f16315694e8
Jose Cardona <jpcardonamena@gmail.com>
2024-06-11 13:51:57 +0200

    Updated Book interface to include all attributes and removed the use of NewBook

--- src/domain/Book.tsx
+++ src/domain/Book.tsx
@@ -1,10 +1,16 @@
 
-import NewBook from "./NewBook";
-
-interface Book extends NewBook {
+interface Book {
     id: string;
     isbn: string;
     userId: number;
+    title: string;
+    subtitle: string;
+    abstract: string;
+    author: string;
+    publisher: string;
+    price: string;
+    numPages: number;
+    cover: string;
 }
 
 export default Book;
